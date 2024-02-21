b8109344ac4d042a302d004fceed6f9e717f58a7
Tan Hao Xiang <Tp060906@mail.apu.edu.my>
2024-02-21 10:21:02 +0800

    Add shadcn ui components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,41 @@
 import "@/styles/globals.css";
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+    getDefaultConfig,
+    RainbowKitProvider,
+  } from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    zora,
+  } from 'wagmi/chains';
+import {
+    QueryClientProvider,
+    QueryClient,
+  } from "@tanstack/react-query";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+const config = getDefaultConfig({
+  appName: 'My RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [mainnet, polygon, optimism, arbitrum, base, zora],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+const queryClient = new QueryClient();
+
+export default function App ({ Component, pageProps }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <Component {...pageProps} />          
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
+
