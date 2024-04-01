aa0b0ed16727c088cc179efcf27044bd85e66c5e
Tan Hao Xiang <haoxiang@coingecko.com>
2024-04-01 19:32:48 +0800

    Add dashboard page

---
+++ src/pages/dashboard.js
@@ -0,0 +1,10 @@
+import Navbar from "@/components/navbar"
+import Sidebar from "@/components/sidebar"
+
+export default function Dashboard() {
+    return (
+        <div>
+           <h1>Test</h1>
+        </div>
+    )
+}
